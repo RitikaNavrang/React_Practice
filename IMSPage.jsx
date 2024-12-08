5958885084ba3b8bf1c43da06b0b4db158a1e674
RitikaNavrang <107111776+RitikaNavrang@users.noreply.github.com>
2024-12-08 15:39:36 +0530

    IMSPage

---
+++ IMSPage.jsx
@@ -0,0 +1,104 @@
+export default function IMSPage()
+{
+    return (<>
+    <h4 style={{textAlign:"center", marginTop:"20px;", marginBottom:"0;", paddingBottom:"0;",backgroundColor:"red;"}}>IMSPage</h4>
+    <table class="table table-striped">
+    <tbody>
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+    </tbody>
+    </table>
+    <h4 style={{marginBottom:"0;", paddingBottom:"0;", backgroundColor:"red;", textAlign:"center"}}>IMSPage</h4>
+    <table class="table table-striped">
+    <tbody>
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+    </tbody>
+    </table><h4 style={{marginBottom:"0;", paddingBottom:"0;", backgroundColor:"red;", textAlign:"center"}}>IMSPage</h4>
+    <table class="table table-striped">
+    <tbody>
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+    </tbody>
+    </table>
+    <h4 style={{marginBottom:"0;", paddingBottom:"0;", backgroundColor:"red;", textAlign:"center"}}>IMSPage</h4>
+    <table class="table table-striped">
+    <tbody>
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 2</td>
+            <td>Cell 1</td>
+        </tr>
+        <tr>
+            <td>Cell 3</td>
+            <td>Cell 1</td>
+        </tr>
+    </tbody>
+    </table>
+    </>)
+}
